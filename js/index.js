c5c86dc07998d7b8949a7e2685f7078d3cc3a966
SIRMly <429625819@qq.com>
2017-06-09 16:05:01 +0800

    ##

---
+++ js/index.js
@@ -0,0 +1,106 @@
+/**
+ * Created by Administrator on 2017/5/26.
+ */
+;(function ($){
+    showDate();
+    changeFS();
+    $(window).resize(function (){
+        changeFS();
+        topHeight();
+    });
+    /*==显示日期==*/
+    function showDate(){
+        var newDate = new Date();
+        var year = newDate.getFullYear();
+        var month = newDate.getMonth()+1;
+        month = month < 10 ? "0"+ month : month;
+        var day = newDate.getDate();
+        day = day < 10 ? "0"+ day : day;
+        $("#date-year").html(year);
+        $("#date-day").html(month+"月"+day+"日");
+    }
+    /*==字体大小==*/
+    function changeFS(){
+        var dateHeight = $("#date").height();
+        var yearheight = dateHeight*0.5;
+        var dayheight = dateHeight*0.27;
+        $("#date-year").css("font-size",yearheight+"px");
+        $("#date-day").css("font-size",dayheight+"px");
+
+    }
+
+
+    /*==part2==*/
+    topHeight();
+    function topHeight(){
+        setTimeout(function (){
+            var topHeight = $("#header").outerHeight();
+            $("#wrapper").css("margin-top",topHeight);
+        },0);
+    }
+    var swiper = new Swiper('.swiper-container',{
+        simulateTouch: false,
+        pagination: '.swiper-pagination',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 0,
+        loop: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+
+
+
+
+    /*==video==*/
+    var video = document.getElementById("video");
+    var video_btn = $("#video-btn");
+    video_btn.addClass("hide");
+    video.addEventListener("click", function () {
+        if (video_btn.hasClass("hide")) {
+            video.pause();
+            video_btn.removeClass("hide");
+        }
+    });
+    video_btn.on("click", function (){
+        video.play();
+        video_btn.addClass("hide");
+    });
+
+    /*==回到顶部==*/
+    $("#toTop").on("click", function () {
+        $("body,html").stop().animate({
+            "scrollTop": 5
+        }, 600);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})(jQuery);
